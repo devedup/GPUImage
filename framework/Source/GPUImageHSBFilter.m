9628c76f58f29973a9069788857567a9e29153a2
Brad Larson <larson@sunsetlakesoftware.com>
2013-10-22 09:16:54 -0500

    Removed unused functions that were throwing errors in the latest LLVM.

--- framework/Source/GPUImageHSBFilter.m
+++ framework/Source/GPUImageHSBFilter.m
@@ -154,41 +154,6 @@ float rscale, gscale, bscale;
 }
 
 /*
- *	lummat -
- *		make a luminance marix
- */
-//static void lummat(mat)
-//float mat[4][4];
-//{
-//    float mmat[4][4];
-//    float rwgt, gwgt, bwgt;
-//    
-//    rwgt = RLUM;
-//    gwgt = GLUM;
-//    bwgt = BLUM;
-//    mmat[0][0] = rwgt;
-//    mmat[0][1] = rwgt;
-//    mmat[0][2] = rwgt;
-//    mmat[0][3] = 0.0;
-//    
-//    mmat[1][0] = gwgt;
-//    mmat[1][1] = gwgt;
-//    mmat[1][2] = gwgt;
-//    mmat[1][3] = 0.0;
-//    
-//    mmat[2][0] = bwgt;
-//    mmat[2][1] = bwgt;
-//    mmat[2][2] = bwgt;
-//    mmat[2][3] = 0.0;
-//    
-//    mmat[3][0] = 0.0;
-//    mmat[3][1] = 0.0;
-//    mmat[3][2] = 0.0;
-//    mmat[3][3] = 1.0;
-//    matrixmult(mmat,mat,mat);
-//}
-
-/*
  *	saturatemat -
  *		make a saturation marix
  */
@@ -234,38 +199,6 @@ float sat;
     mmat[3][3] = 1.0;
     matrixmult(mmat,mat,mat);
 }
-
-/*
- *	offsetmat -
- *		offset r, g, and b
- */
-//static void offsetmat(mat,roffset,goffset,boffset)
-//float mat[4][4];
-//float roffset, goffset, boffset;
-//{
-//    float mmat[4][4];
-//    
-//    mmat[0][0] = 1.0;
-//    mmat[0][1] = 0.0;
-//    mmat[0][2] = 0.0;
-//    mmat[0][3] = 0.0;
-//    
-//    mmat[1][0] = 0.0;
-//    mmat[1][1] = 1.0;
-//    mmat[1][2] = 0.0;
-//    mmat[1][3] = 0.0;
-//    
-//    mmat[2][0] = 0.0;
-//    mmat[2][1] = 0.0;
-//    mmat[2][2] = 1.0;
-//    mmat[2][3] = 0.0;
-//    
-//    mmat[3][0] = roffset;
-//    mmat[3][1] = goffset;
-//    mmat[3][2] = boffset;
-//    mmat[3][3] = 1.0;
-//    matrixmult(mmat,mat,mat);
-//}
 
 /*
  *	xrotate -
